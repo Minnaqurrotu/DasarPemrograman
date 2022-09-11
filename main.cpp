cd23e05a3a895d153c17724c4fce6ad9292b5057
Minnaqurrotu <113005695+Minnaqurrotu@users.noreply.github.com>
2022-09-11 11:41:07 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Minna Qurrotu A'yunina" << endl;
+    cout << "A11.2022.14612" << endl;
+    cout << "Teknik Informatika" << endl;
+    cout << "Kendal, 15 Juni 2004" << endl;
+    cout << "Desa Tosari Dukuh Siwalan RT 3 RW 2 Kec. Brangsong Kab. Kendal" << endl;
+    cout << "Hobi mendengarkan musik" << endl;
+    return 0;
+}
