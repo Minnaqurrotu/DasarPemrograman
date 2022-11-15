58ee56b60db00002d4e783738eb6e6b100fa3bbf
Minnaqurrotu <113005695+Minnaqurrotu@users.noreply.github.com>
2022-11-15 23:03:12 +0700

    Add files via upload

---
+++ Pertemuan 11/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*cout << "Bilangan Genap 1-100" << endl;
+    cout << endl;
+    for (int genap = 1; genap <= 100; genap++) {
+        if (genap % 2 == 0) {
+            cout << genap << endl;
+        }
+    }
+    cout << endl;
+    cout << "Bilangan Ganjil 1-100" << endl;
+    cout << endl;
+    for (int ganjil = 1; ganjil <= 100; ganjil++) {
+        if (ganjil % 2 == 1) {
+            cout << ganjil << endl;
+        }
+    }
+    cout << endl;
+    cout << "Bilangan Kelipatan 6 dari 1-100" << endl;
+    cout << endl;
+    for (int kelipatan6 = 6; kelipatan6 <= 100; kelipatan6++) {
+        if (kelipatan6 % 6 == 0) {
+            cout << kelipatan6 << endl;
+        }
+    }*/
+
+    for (int i = 0; i < 2; i++) {
+        cout << i << endl;
+        for (int j = 0; j < 3; j++) {
+            cout << j << endl;
+        }
+    }
+    return 0;
+}
