9628aa7ec916468a8f588ad94175e8596dade4a7
Minnaqurrotu <113005695+Minnaqurrotu@users.noreply.github.com>
2022-11-27 23:51:43 +0700

    Add files via upload

---
+++ Problem_PDP-05/loop11/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 1; i <= 10; i++){
+        int x = i*7;
+        cout << i << " x " << 7 << " = " << x << endl;
+    }
+    return 0;
+}
