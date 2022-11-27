3d2968560c5062434feee54a9850ca818c0a78c6
Minnaqurrotu <113005695+Minnaqurrotu@users.noreply.github.com>
2022-11-27 23:42:51 +0700

    Add files via upload

---
+++ Problem_PDP-05/loop1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i = 100, j = 1; i >= 1, j <= 100; i--, j++){
+        cout << i << " dan " << j << endl;
+    }
+    return 0;
+}
